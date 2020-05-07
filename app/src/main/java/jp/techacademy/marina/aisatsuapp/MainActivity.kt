6c3e6570012349eeacd0fdc00aab220005c7a86f
marina tanaka <tanakamarina094@gmail.com>
2020-05-07 17:07:27 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/marina/aisatsuapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package jp.techacademy.marina.aisatsuapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.app.TimePickerDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button1) {
+            showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                Log.d("UI_PARTS", "$hour:$minute")
+            },
+            13, 0, true
+        )
+        timePickerDialog.show()
+    }
+
+     fun greeting(hour:Int) {
+        if ( 2 <=hour && hour <10) {
+            textView.text = "おはよう"
+        } else if ( hour < 18) {
+            textView.text = "こんにちは"
+        } else {
+            textView.text = "こんばんは"
+        }
+    }
+    }
+
